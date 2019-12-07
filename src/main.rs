660348b484d65bda0b82882e24b7d6e2b75fc776
Maxime Deboffle <maxime.deboffle@expaceo.com>
2019-12-07 12:02:34 +0100

    Setting up clap library

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+extern crate clap;
+use clap::App;
+
+fn main() { 
+    App::new("mypipe")
+       .version("1.0")
+       .about("Does great things!")
+       .author("Maxime D.")
+       .get_matches(); 
 }
